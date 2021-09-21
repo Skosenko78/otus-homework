dbc1f82ddf307240fe115b7ad8aa909da504e45a
Sergey Kosenko <kosenko_sergei@mail.ru>
2021-09-21 13:48:33 +0300

    LAB8

--- Lab8Files/script.sh
+++ Lab8Files/script.sh
@@ -8,12 +8,20 @@ TMPFILE='mailreport.txt'
 CURRENTDATE='14 Aug 2019 05:00 +0300'
 REPORTDT="$(date -d "$CURRENTDATE" '+%d/%b/%G:%H')"
 
+function dateconv {
+
+echo $(date -d "$CURRENTDATE + $1 hour" '+%d/%b/%G:%T %z')
+
+}
+
 if [ -f $TMPFILE ]; then
-	echo "Script is running."
-	exit 0
+	echo "Script is already running."
+	exit 1
 fi
 
-echo "EVENTS from $(date -d "$CURRENTDATE" '+%d/%b/%G:%T %z') to $(date -d "$CURRENTDATE +1 hour" '+%d/%b/%G:%T %z')" > $TMPFILE
+trap 'rm -f $TMPFILE' exit
+
+echo "EVENTS from $(dateconv 0) to $(dateconv 1)" > $TMPFILE
 echo "Top 20 Source IP addresses:" >> $TMPFILE
 grep $REPORTDT $LOGFILE | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | head -20 | awk 'BEGIN {print "Count    IP address"} {print $1 "\t" $2}' >> $TMPFILE
 echo "Top 20 Quered addresses:" >> $TMPFILE
@@ -23,5 +31,4 @@ grep $REPORTDT $LOGFILE | grep -oE ' [1-5][0-9]{2} ' | sort | uniq -c |  awk 'BE
 echo "Error entries:" >> $TMPFILE
 grep $REPORTDT $LOGFILE | grep -E ' [3-5][0-9]{2} ' | sort +4 >> $TMPFILE
 mail -s Report $MAILTO < $TMPFILE
-rm -f $TMPFILE
 exit 0
